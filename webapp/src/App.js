d9f44460549eb1490eb310db85bd8a6c2286f16e
celinechole <github@celinechole.com>
2020-03-08 20:17:44 -0400

    install chakra UI

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
+import {ThemeProvider, CSSReset} from '@chakra-ui/core'
 
 function App() {
   return (
-    <div>
+    <ThemeProvider>
+      <CSSReset />
       <button onClick={async () => {
         const yo = await fetch("/api")
         alert(await yo.text())
         }}>Click Me</button>
-    </div>
+    </ThemeProvider>
   );
 }
 
