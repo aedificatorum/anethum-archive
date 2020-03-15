ca6aee84a101df0eee8ae11f3800a74dba1224d5
celinechole <github@celinechole.com>
2020-03-15 12:02:58 -0400

    connect api to db

--- api/index.js
+++ api/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const { Pool } = require("pg");
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+
+const pool = new Pool({
+  connectionString
+})
 
 const app = express();
 app.use(express.static('public'))
 
 app.get('/api', (req, res, next) => {
   res.send('hello')
+})
+
+app.get('/db', async (req, res, next) => {
+  const result = await pool.query('select * from test')
+  res.json(result.rows)
 })
 
 const port = process.env.PORT || 8881;
